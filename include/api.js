1032c99b164a08a85ccf14b59153d4d1606e3e02
bre1470 <40056287+bre1470@users.noreply.github.com>
2019-01-25 19:52:39 +0100

    Switched to new search.

--- include/api.js
+++ include/api.js
@@ -68,7 +68,7 @@ hapi.ajax = function(p) {
     if (props.dataType === 'json') {
       var json;
       try {
-        json = JSON.parse(r.responseText);
+        json = JSON.parse(/[\[\{].*[\]\}]/.exec(r.responseText)[0]);
       } catch (e) {
         props.error(e.stack, r.responseText);
         return;
@@ -917,46 +917,38 @@ hapi.ajax = function(p) {
       } else {
         clearSideResults();
         clearTextResults();
-        loadSideSuggestions();
+        // loadSideSuggestions();
       }
     }
 
-    function searchText(e, offset) {
+    function searchText(e, page) {
       if (e.keyCode !== 13 &&
-        typeof offset === 'undefined'
+        typeof page === 'undefined'
       ) {
         return;
       }
-      offset = (offset || 0);
-      if (offset <= 0) {
+      page = (page || 1);
+      if (page < 2) {
         clearTextResults();
       }
       hapi.ajax({
         dataType: 'json',
-        headers: {
-          'Ocp-Apim-Subscription-Key': '-'
-        },
         url: (
-          'https://api.cognitive.microsoft.com' +
-          '/bingcustomsearch/v7.0/search' +
-          '?q=' + encodeURIComponent(query) +
-          '&count=' + maxElements +
-          '&customconfig=1554546297' +
-          '&mkt=en-US' +
-          '&offset=' + offset +
-          '&safesearch=Strict' +
-          '&textDecorations=true' +
-          '&textFormat=HTML'
+          'https://api.highcharts.com/search/search.php' +
+          '?query=' + encodeURIComponent(query) +
+          '&type=and' +
+          '&start=' + page +
+          '&results=' + maxElements +
+          '&search=1'
         ),
         success: function(json) {
-          if (!json.queryContext ||
-            json.queryContext.originalQuery !== query
+          if (!json ||
+            json.query !== query ||
+            json.total_results === 0
           ) {
             return;
           }
-          showTextResults(
-            (json.webPages || { totalEstimatedMatches: 0, value: [] }), query
-          );
+          showTextResults(json, query);
         }
       });
     }
@@ -991,7 +983,7 @@ hapi.ajax = function(p) {
         sideResults.scrollTo(1, 0);
       } else {
         sideResults.style.display = 'none';
-        loadSideSuggestions();
+        // loadSideSuggestions();
       }
     }
 
@@ -1008,20 +1000,16 @@ hapi.ajax = function(p) {
       }
       loadSideSuggestionsTimeout = window.setTimeout(hapi.ajax, 500, {
         dataType: 'json',
-        headers: {
-          'Ocp-Apim-Subscription-Key': '-'
-        },
         url: (
-          'https://api.cognitive.microsoft.com' +
-          '/bingcustomsearch/v7.0/suggestions/search' +
+          'https://api.highcharts.com/search/include/js_suggest/suggest.php' +
           '?q=' + encodeURIComponent(query) +
-          '&customconfig=1554546297'
+          '&type=query'
         ),
         success: function(json) {
-          if (json.suggestionGroups &&
-            json.suggestionGroups.length > 0
+          if (json &&
+            json.length > 0
           ) {
-            showSideSuggestions(json.suggestionGroups[0].searchSuggestions);
+            showSideSuggestions(json);
           }
         }
       });
@@ -1031,14 +1019,14 @@ hapi.ajax = function(p) {
       var a,
         suggestion;
       for (var i = 0, ie = suggestions.length; i < ie; ++i) {
-        suggestion = suggestions[i];
+        suggestion = suggestions[i][0];
         a = cr('a', null, suggestion.displayText);
         a.setAttribute('href', '#');
         on(a, 'click', function (e) {
           e.preventDefault();
           clearTextResults();
           query = this.innerText;
-          searchText(e, 0);
+          searchText(e);
         });
         ap(sideResults, ap(cr('li', 'match'), a));
         if (sideResults.childElementCount >= maxElements) {
@@ -1063,34 +1051,37 @@ hapi.ajax = function(p) {
       stopSideSuggestions();
       var a,
         div,
-        entries = json.value,
+        entries = (json.qry_results || []),
         entry,
         name,
-        url,
-        snippet;
+        next = (json.next || 2),
+        pages = (json.pages || 0),
+        snippet,
+        total = (json.total_results || 0),
+        url;
       for (var i = 0, ie = entries.length; i < ie && i < maxElements; ++i) {
         entry = entries[i];
-        name = (entry.name || '');
+        name = (entry.title || '');
         url = (entry.url || '/');
-        snippet = (entry.snippet || '');
-        if (name.indexOf('|') > 0) {
+        snippet = (entry.fulltxt || '');
+        if (name.indexOf('|') > -1) {
           name = name.substr(0, name.indexOf('|'));
         }
-        if (snippet.indexOf('Welcome') === 0) {
+        if (name.indexOf('Highcharts') === 0) {
+          name = name.substr(11);
+        }
+        if (name.indexOf(':') === -1) {
+          name = 'Option: ' + name;
+        }
+        if (snippet.indexOf('Welcome') === 0 ||
+          snippet.indexOf('<b>') === -1
+        ) {
           snippet = '';
         }
         a = cr('a', null, name, true);
         a.setAttribute('href', url);
         a.setAttribute('title', 'Go to ' + url.substr(url.indexOf('//') + 2));
-        a.setAttribute('data-crawl',
-          entry.dateLastCrawled.substr(0, entry.dateLastCrawled.indexOf('T'))
-        );
         div = cr('div', 'match');
-        if (url.lastIndexOf('/') === (url.length - 1) ||
-          url.indexOf('.json') >= 0
-        ) {
-          div.setAttribute('style', 'display: none;');
-        }
         ap(textResults,
           ap(div,
             ap(
@@ -1117,10 +1108,8 @@ hapi.ajax = function(p) {
         textResults.style.display = 'block';
         scrollTo(textResults, textResults.firstChild, 200);
       }
-      var foundText = 'Found ' + pluralize(
-        json.totalEstimatedMatches, ' result', ' results'
-      );
-      if (json.totalEstimatedMatches > textResults.childNodes.length) {
+      var foundText = 'Found ' + pluralize(total, ' result', ' results');
+      if (next <= pages) {
         var divOptions = cr('div', 'options')
         var aClose = cr('span', 'close', 'Close results');
         on(aClose, 'click', function (e) {
@@ -1132,7 +1121,7 @@ hapi.ajax = function(p) {
         on(aMore, 'click', function (e) {
           e.preventDefault();
           textResults.removeChild(divOptions);
-          searchText(e, textResults.childNodes.length);
+          searchText(e, next);
         });
         ap(
           textResults,
